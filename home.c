835338bb2c8b926b0d54fa8d3ae20720c8ae296c
damyoungtechnologies <damyounggraphics@gmail.com>
2023-08-15 22:06:49 +0100

    Nothing is here

---
+++ home.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+	//Thia is for a start
+	printf("Hello Damyoung\n\n");
+        printf("Am i good to start\n");
+	printf("I am a graduate of \t the University of Nigeria, Nsukka\n\n");
+	printf("I am a software engineer \\ I am a full stack website developer. \"Bravo\".");
+	return 0;
+	/*This is the end of
+	 * the code here*/
+}
