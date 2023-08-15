3287a86a7b145489bbf4fee223807b68ad77b9fe
damyoungtechnologies <damyounggraphics@gmail.com>
2023-08-15 22:09:56 +0100

    W3 school practice is here

---
+++ new_line.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main(){
+	int man = 10;
+	int manny = 20;
+	int logo = man + manny;
+	printf("I am %i years old\n", logo);
+	printf("My brother is %d years old\n", manny);
+	float num0 = 0.9402;
+	float num1 = 0.6502;
+	float num2 = num0 + num1;
+	float num3 = num0 + num2;
+	printf("This is the result: %f + (%f * %f)\n", num0, num0, num1);
+	printf("%f\n", num2);
+        printf("\n%f", num3);
+	char first = 'S';
+	char second = 'P';
+	printf("\nThe first born initial is %c and the second born initial is %c.", first, second);
+	return 0;
+}
